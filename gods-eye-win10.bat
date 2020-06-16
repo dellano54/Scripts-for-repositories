b7c16e28cbfa6d7d0c73a948b7d84eda45624e03
dellano samuel fernandez <62639153+dellano54@users.noreply.github.com>
2020-06-16 08:06:09 +0000

    Delete gods-eye-win10.bat

--- gods-eye-win10.bat
+++
@@ -1,5 +0,0 @@
-mkdir "c:\Program Files\gods-eye-script\"
-"bitsadmin /transfer myDownloadJob /download /priority normal https://www.7-zip.org/a/7z1900.exe c:\Program Files\gods-eye-script\"
-"bitsadmin /transfer myDownloadJob /download /priority normal https://github.com/dellano54/gods-eye-for-windows/archive/master.zip c:\Program Files\gods-eye-script\"
-cd "C:\Program Files\gods-eye-scripts"
-"C:\Program Files\gods-eye-script\7z*.exe" a master.zip -r "C:\Program Files\gods-eye-script\"
